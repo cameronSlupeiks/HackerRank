a82a767257ed320a8934d14cc45de275360808de
Cameron Slupeiks <cslupeiks@gmail.com>
2019-03-08 14:00:35 -0500

    Added new solution

---
+++ Languages/Python/NestedLists.py
@@ -0,0 +1,35 @@
+
+# Challenge Link: https://www.hackerrank.com/challenges/nested-list/problem
+
+if __name__ == '__main__':
+
+    students = []
+    scores = []
+    secondLowest = []
+    current = 0
+
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        student = [name, score]
+        students.append(student)
+        scores.append(score)
+
+    scores.sort()
+
+    for i in range(len(scores)):
+        if len(scores) == 1:
+            secondLowestScore = scores[current]
+        elif scores[current] < scores[current+1]:
+            secondLowestScore = scores[current+1]
+        else:
+            current += 1
+    
+    for i in range(len(students)):
+        if students[i][1] == secondLowestScore:
+            secondLowest.append(students[i][0])
+    
+    secondLowest.sort()
+
+    for i in range(len(secondLowest)):
+        print(secondLowest[i])
