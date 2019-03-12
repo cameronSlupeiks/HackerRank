265c045ead2de05698dc1a2e09712c296bd974c6
Cameron Slupeiks <cslupeiks@gmail.com>
2019-03-12 12:45:52 -0400

    Added New Solution

---
+++ Data Structures/Linked Lists/ReversePrint.js
@@ -0,0 +1,92 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep) {
+    while (node != null) {
+        process.stdout.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            process.stdout.write(sep);
+        }
+    }
+}
+
+function reversePrint(head) {
+    let current = head;
+    let array = [];
+
+    while (current != null) {
+        array.push(current.data);
+        current = current.next;
+    }
+
+    array = array.reverse();
+
+    for (let i = 0; i < array.length; i++) {console.log(array[i]);}
+}
+
+function main() {
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const llistCount = parseInt(readLine(), 10);
+
+        let llist = new SinglyLinkedList();
+
+        for (let i = 0; i < llistCount; i++) {
+            const llistItem = parseInt(readLine(), 10);
+            llist.insertNode(llistItem);
+        }
+
+        reversePrint(llist.head);
+    }
+}
