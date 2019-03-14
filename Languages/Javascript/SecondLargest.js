d7d2b6e6fb8bdeb52fa1974847ccdfe6df508bd4
Cameron Slupeiks <cslupeiks@gmail.com>
2019-03-14 16:18:46 -0400

    Create SecondLargest.js

---
+++ Languages/Javascript/SecondLargest.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getSecondLargest(nums) {
+
+    let uniqueNums = nums.filter((num, i) => {
+        return nums.indexOf(num) >= i;
+    });
+
+    
+    return uniqueNums.sort(function (a, b) { return b - a })[1];  
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
