be1daf7a870f67e648ca1f80212548bec1302dff
Cameron Slupeiks <cslupeiks@gmail.com>
2019-04-08 13:27:25 -0400

    Create InsertNode.js

---
+++ Data Structures/Linked Lists/InsertNode.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+function insertNodeAtPosition(head, data, position) {
+
+    let counter = 0;
+    let current = head;
+    let node = new SinglyLinkedListNode(data);
+
+    if (current === null) { return node; }
+    if (position == 0) {node.next = head; return node;}  
+
+    while (counter < position - 1 && head.next != null) {
+        head = head.next;
+        counter++;
+    }
+
+    let nodeAtPosition = head.next;
+    head.next = node;
+    head = head.next;
+    head.next = nodeAtPosition;
+
+    return current;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const llistCount = parseInt(readLine(), 10);
+
+    let llist = new SinglyLinkedList();
+
+    for (let i = 0; i < llistCount; i++) {
+        const llistItem = parseInt(readLine(), 10);
+        llist.insertNode(llistItem);
+    }
+
+    const data = parseInt(readLine(), 10);
+
+    const position = parseInt(readLine(), 10);
+
+    let llist_head = insertNodeAtPosition(llist.head, data, position);
+
+    printSinglyLinkedList(llist_head, " ", ws)
+    ws.write("\n");
+
+    ws.end();
+}
